d23834e2d145f71edf2b598f07f7fdc876acf630
ssolanki <sushobhitsolanki@gmail.com>
2018-07-13 17:00:11 +0530

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
   name = 'bimap',
   packages = ['bimap'],
-  version = '0.1.0',
+  version = '0.1.1',
   description = 'Container similar to boost bimap',
   author = 'Sushobhit',
   author_email = 'sushobhitsolanki@gmail.com',
